c745d3f38ec7387dfdb1510b3ea145ceb017081e
Will Wadman <wadmanwill@gmail.com>
2022-12-13 13:23:20 +0000

    add solutions to 6 & 7

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,8 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
 Store.create(name: "Burnaby", annual_revenue: 300000, womens_apparel: true, mens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true, mens_apparel: false)
 Store.create(name: "Gastown", annual_revenue: 190000, womens_apparel: false, mens_apparel: true)
+
+#test invalid name length
+Store.create(name: 'h', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+#test invalid revenue
+Store.create(name: 'invalidRevenue', annual_revenue: 0, mens_apparel: true, womens_apparel: false)
+
+#test invalid apparel type
+Store.create(name: 'invalidApparel', annual_revenue: 1000000, mens_apparel: false, womens_apparel: false)
 
 puts "There are " + Store.count.to_s + " stores."
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Jonathan", last_name: "Sue", hourly_rate: 1000000)
+@store1.employees.create(first_name: "Jobu", last_name: "Cleve", hourly_rate: 52)
+@store1.employees.create(first_name: "Cranky", last_name: "Danks", hourly_rate: 9)
+@store1.employees.create(first_name: "Mufasa", last_name: "Lion", hourly_rate: 104)
+@store1.employees.create(first_name: "Dangle", last_name: "Lt", hourly_rate: 512)
+@store2.employees.create(first_name: "Sipper", last_name: "Von dipper", hourly_rate: 14)
+@store2.employees.create(first_name: "George Michael", last_name: "Bluth", hourly_rate: 00)
+@store2.employees.create(first_name: "Uncle", last_name: "Bob", hourly_rate: 83)
+@store2.employees.create(first_name: "Randy", last_name: "Bobandy", hourly_rate: 1)
+@store2.employees.create(first_name: "Ricky", last_name: "Isms", hourly_rate: 36)
